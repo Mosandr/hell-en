f639cdee57d8496752b06d7f4d462a8ce0f9b2fd
Andriy Mosol <40470899+Mosandr@users.noreply.github.com>
2021-01-06 23:49:24 +0200

    Updates

--- src.e31bb0bc.js
+++ src.e31bb0bc.js
@@ -189,7 +189,7 @@ var reloadCSS = require('_css_loader');
 
 module.hot.dispose(reloadCSS);
 module.hot.accept(reloadCSS);
-},{"_css_loader":"../node_modules/parcel-bundler/src/builtins/css-loader.js"}],"index.js":[function(require,module,exports) {
+},{"./..\\images\\hero-bg-m.png":[["hero-bg-m.603c742c.png","images/hero-bg-m.png"],"images/hero-bg-m.png"],"./..\\images\\hero-bg-m@2x.png":[["hero-bg-m@2x.a3bfdd2b.png","images/hero-bg-m@2x.png"],"images/hero-bg-m@2x.png"],"./..\\images\\hero-bg-t.png":[["hero-bg-t.4e0d8af2.png","images/hero-bg-t.png"],"images/hero-bg-t.png"],"./..\\images\\hero-bg-t@2x.png":[["hero-bg-t@2x.e9495549.png","images/hero-bg-t@2x.png"],"images/hero-bg-t@2x.png"],"./..\\images\\hero-bg-d.png":[["hero-bg-d.72892569.png","images/hero-bg-d.png"],"images/hero-bg-d.png"],"./..\\images\\hero-bg-d@2x.png":[["hero-bg-d@2x.77e8983e.png","images/hero-bg-d@2x.png"],"images/hero-bg-d@2x.png"],"_css_loader":"../node_modules/parcel-bundler/src/builtins/css-loader.js"}],"index.js":[function(require,module,exports) {
 "use strict";
 
 require("./sass/main.scss");
@@ -221,7 +221,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "64715" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "58022" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
